4994f99db975606be2d2e837e398a7bb19403926
Tauseef Khan <tauseefbhai85@example.com>
2026-03-18 23:58:31 +0530

    remove api key

--- BuyZone/settings.py
+++ BuyZone/settings.py
@@ -100,15 +100,17 @@ USE_TZ = True
 # ======================
 # EMAIL
 # ======================
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
-EMAIL_HOST_USER = 'tauseefkhan3738@gmail.com'
-EMAIL_HOST_PASSWORD = 'xcrmhxbwtbtibjaq'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_TIMEOUT = 10
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = ''  # 👈 tera API key
+
+DEFAULT_FROM_EMAIL = 'tauseefkhan3738@gmail.com'
 
 # ======================
 # STATIC & MEDIA
